29b29cb94b05cc6228ab009169b193cbcbe3f096
Henry lien <129733971+Henry0811111@users.noreply.github.com>
2026-06-06 19:08:10 +0200

    EF core migration

--- FamilyGames.Domain/obj/Debug/net10.0/FamilyGames.Domain.AssemblyInfo.cs
+++ FamilyGames.Domain/obj/Debug/net10.0/FamilyGames.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyGames.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fb4a6ee6c1948b459e13a401e562f84064c360")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyGames.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyGames.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
